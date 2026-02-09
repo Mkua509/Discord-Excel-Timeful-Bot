917e8bbf106226b744ed37310d44a912e4561971
Mkua509 <maternuskuang@gmail.com>
2026-02-09 17:16:32 +1300

    Added a loop feature based off time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import logging
 from dotenv import load_dotenv
 import os
@@ -33,7 +33,30 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f"We are ready to go in {bot.user.name}")
+    reminder_loop.start()
 
+# Reminds people based on a loop
+@tasks.loop(seconds=5)
+async def reminder_loop():
+    # Fetch fresh data from spreadsheet each time
+    values_list = worksheet.col_values(1) 
+    completed_list = worksheet.col_values(2)
+    discord_id = worksheet.col_values(3)
+    
+    # Remove headers
+    values_list.pop(0)
+    completed_list.pop(0)
+    discord_id.pop(0)
+
+    # Uses Zip to ensure it doesn't over iterate
+    for values_list, completed_list, discord_id in zip(values_list, completed_list, discord_id):
+        user_id = int(discord_id)
+        user = await bot.fetch_user(user_id)
+        if completed_list == 'FALSE':
+            await user.send(f"Please fill out the {excel_name} form :((")
+    
+
+# Reminds people based on a command
 @bot.command()
 async def filled_form(ctx):
     # Fetch fresh data from spreadsheet each time
@@ -52,7 +75,7 @@ async def filled_form(ctx):
             user_id = int(discord_id)
             #await ctx.send(f"<@{user_id}> Please fill out the form :((") old comment for sending in serer
             user = await bot.fetch_user(user_id)
-            await user.send(f"Please fill out the form :((")
+            await user.send(f"Please fill out the {excel_name} form :((")
     
     await ctx.send("DMs sent to everyone who hasn't filled the form!")
 
